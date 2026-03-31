e2f08a342328118f6b1165afadb9bfe47d5f10c1
Fábio Mattes <fabiomattes2007@gmail.com>
2026-03-31 17:31:18 -0300

    Primeiro commit

---
+++ src/cfehome/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from cfehome.views import home_page
+
+
+urlpatterns = [
+    path('', home_page, name='home'),
+    path('admin/', admin.site.urls),
+]
---
+++ src/visits/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import PageVisit
+
+
+admin.site.register(PageVisit)
+
